8c6292f092ecab0f5e3084b2687ba41269e2c700
rykcbaoolNEW <165583582+rykcbaoolNEW@users.noreply.github.com>
2026-05-30 20:01:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,35 +3,28 @@ const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-const webhookURL = "https://discord.com/api/webhooks/1330532752562589729/w3hY8Arl70VuXEolV4Thmrb5Tr-3aMZ5W_mqd5NX0jA-7hnXoaF2xrNhsmv4xBNQ54nR";
-const tpaLogger = "https://discord.com/api/webhooks/1330661283304968307/KMqyjhKNC7-u6MD-1FMPI1Zh8Y1qWb6f8xWNwNu5z1vaeUe-Uro0g5Aoy317REOCblbQ";
 const allowed = ["ryk_cbaool", ".JBTMojang", "Meacia", "saviors_on_top", "ryk_cool"]; // DO NOT EDIT
 const host = "pvp.duckcraft.top";
 const port = 25565;
-const maxBots = 10; // Define the maximum number of bots
+const maxBots = 10;
+const username = 'ryk_isfarming';
 let botCount = 0;
-const usernames = [
-  "CoolDudeBot919",
-  "CoolDudeBot976",
-  "CoolDudeBot880",
-  "CoolDudeBot930",
-  "CoolDudeBot743"
-];
+
 
 function createBot() {
     if (botCount >= maxBots) return;
 
     const bot = mineflayer.createBot({
-    host: host,
-    port: port,
-    username: usernames[Math.floor(Math.random() * usernames.length)],
-    auth: "offline",
-    version: "1.20.1",
-    keepAlive: true,
-    checkTimeoutInterval: 60000
-});
+        host: host,
+        port: port,
+        username: username,
+        auth: "offline",
+        version: "1.20.1",
+        keepAlive: true,
+        checkTimeoutInterval: 60000
+    });
     let hasSpawnedOnce = false;
-    
+
     bot.once("spawn", async () => {
         bot.chat("/login Savior");
         bot.setControlState("forward", true); // Move forward on spawn
@@ -73,12 +66,12 @@ function createBot() {
         }
     });
 
-  bot.on("kicked", (reason, loggedIn) => {
-    console.log(`❌ Bot ${bot.username || "Unknown"} was kicked`);
-    console.log(`Reason:`, reason);
-    console.log(`LoggedIn: ${loggedIn}`);
-});
-    
+    bot.on("kicked", (reason, loggedIn) => {
+        console.log(`❌ Bot ${bot.username || "Unknown"} was kicked`);
+        console.log(`Reason:`, reason);
+        console.log(`LoggedIn: ${loggedIn}`);
+    });
+
     bot.on("end", () => {
         console.log(`Bot ${bot.username || "Unknown"} disconnected. Reconnecting in 10 seconds...`);
         setTimeout(createBot, 10000); // Reconnect after 10 seconds
@@ -87,8 +80,5 @@ function createBot() {
     botCount++;
 }
 
-// **Loop to create multiple bots**
-for (let i = 0; i < maxBots; i++) {
-    setTimeout(createBot, i * 5000); // Spawn each bot with a 5-second delay to prevent rate limiting
-}
+setTimeout(createBot); // Spawn each bot with a 5-second delay to prevent rate limiting
 
